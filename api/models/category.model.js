b1ec81474923e4719f89e5fc42712a461ac26554
basebandit <8973567+basebandit@users.noreply.github.com>
2018-10-26 17:47:04 +0300

    Category collection schema

---
+++ api/models/category.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    title: { type: String, unique: true }
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
