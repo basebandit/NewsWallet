f02554bd11f421ed2745eaeae9f6e1c9758a1832
basebandit <8973567+basebandit@users.noreply.github.com>
2018-10-29 19:51:52 +0300

    Added logic for checking already existing categories

--- api/controllers/article.controller.js
+++ api/controllers/article.controller.js
@@ -1,21 +1,33 @@
 const Article = require("../models/article.model");
-// const Category = require("../models/category.model");
+const Category = require("../models/category.model");
 const log = require("simple-node-logger").createSimpleLogger();
 
 exports.createArticle = async (req, res) => {
     log.info(req.file);
     try {
-        let article = new Article({
-            title: req.body.title,
-            description: req.body.description,
-            author: req.body.author,
-            articleImage: req.file.path || "",
-            origin: req.body.origin,
-            originUrl: req.body.originUrl,
-            categoryList: req.body.category
-        });
+    //Lets check if this category already exists before creating it
+        let article;
+        if (req.body.category) {
+            let category = await Category.findOne({ title: req.body.category });
+            if (category) {
+                article = new Article({
+                    title: req.body.title,
+                    description: req.body.description,
+                    author: req.body.author,
+                    articleImage: req.file.path || "",
+                    origin: req.body.origin,
+                    originUrl: req.body.originUrl,
+                    categoryList: category._id
+                });
+            } else {
+                category = new Category({
+                    title: req.body.category
+                });
 
-        article = await article.save();
+                category = await category.save();
+            }
+            article = await article.save();
+        }
 
         res.status(200).json({
             message: "Saved article successful",
@@ -23,6 +35,6 @@ exports.createArticle = async (req, res) => {
         });
     } catch (err) {
         log.error(err.message);
-        res.status(500).json({ message: "Error when saving article" });
+        res.status(400).json({ message: "already exists" });
     }
 };
