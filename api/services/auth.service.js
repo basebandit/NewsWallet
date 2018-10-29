d63987862036a22c285cc72271b719dc413d1345
basebandit <8973567+basebandit@users.noreply.github.com>
2018-10-29 19:48:09 +0300

    Generate access token

---
+++ api/services/auth.service.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("../../config");
+const User = require("../models/user.model");
+
+exports.verify = (req, res, next) => {
+    let token = req.headers["authorization"];
+    if (token && token.indexOf("Bearer") !== -1) {
+        try {
+            token = token.split(" ")[1].trim();
+            jwt.verify(token, config.authentication.secret, async (err, decoded) => {
+                const { username } = decoded;
+                const user = await User.findOne({ username: username });
+                if (!user) {
+                    return res.status(404).json({ message: "not found" });
+                }
+                next();
+            });
+        } catch (err) {
+            return res.status(400).json({ message: "Bad request" });
+        }
+    } else {
+        res.status(403).json({ message: "Forbidden" });
+    }
+};
