9f7fa5dc3421b3a74c93f636e76db9c58acc8471
basebandit <8973567+basebandit@users.noreply.github.com>
2018-11-05 13:18:33 +0300

    Added gpl v3.0 preamble

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+/**
+ * NewsWallet Api is a restful application interface for a mobile client
+ * application
+ *
+ * Copyright (C) 2018  Basebandit
+ *
+ * This program is free software: you can *redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const express = require("express");
 const { AssertionError } = require("assert");
 const bodyParser = require("body-parser");
@@ -34,7 +54,6 @@ const port = process.env.PORT;
 const uri = `mongodb://${config.user}:${config.pwd}@${config.host}:${
     config.port
 }/${config.db}`;
-
 
 /**
  * Middlewares
