366e8e274e40483ad8f2a4dcf4e5f275155f5525
basebandit <8973567+basebandit@users.noreply.github.com>
2018-11-05 13:12:23 +0300

    Added description and author

--- api/models/category.model.js
+++ api/models/category.model.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+/**
+ *
+ * @author basebandit
+ *
+ * @description Category schema
+ */
+
 const CategorySchema = new Schema({
     title: { type: String, lowercase: true, unique: true },
     slug: { type: String }
