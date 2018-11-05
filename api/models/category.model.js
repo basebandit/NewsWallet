e11de5bbc3fdbefb00e3360b838ed08606f7bcfd
basebandit <8973567+basebandit@users.noreply.github.com>
2018-11-05 10:17:57 +0300

    Added slug to category to make them user friendly

--- api/models/category.model.js
+++ api/models/category.model.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    title: { type: String, unique: true }
+    title: { type: String, lowercase: true, unique: true },
+    slug: { type: String }
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
